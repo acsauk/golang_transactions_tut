d41ac3a285cf1fdae9dabd11acc8498d016dc8f0
Alex Saunders <acsauk@gmail.com>
2017-05-09 22:27:55 +0100

    removes previous benchmark test

--- fund_test.go
+++ fund_test.go
@@ -45,26 +45,4 @@ func BenchmarkWithdrawals(b *testing.B) {
   if fund.Balance() != 0 {
     b.Error("Balance wasn't zero:", fund.Balance())
   }
-
-}
-
-func BenchmarkFund(b *testing.B) {
-  // Add as many pounds as there are iterations in this run
-  fund := NewFund(b.N)
-
-  // Withdraw the deposit one pound at a time until the balance is 0
-  for i := 0; i < b.N; i++ {
-    fund.Withdraw(1)
-  }
-
-  if fund.Balance() != 0 {
-    b.Error("Balance wasn't zero:", fund.Balance())
-  }
-
-  func() {
-    resource.Lock()
-    defer resource.Unlock()
-
-    // Do stuff with the resource here...
-  }
 }
