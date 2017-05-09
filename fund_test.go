5489da7f6fbebbef27ae6b73f5dae991269b757d
Alex Saunders <acsauk@gmail.com>
2017-05-09 22:24:22 +0100

    writes test for concurrent withdrawals using workers and waitgroups

--- fund_test.go
+++ fund_test.go
@@ -1,6 +1,52 @@
 package funding
 
-import "testing"
+import (
+  "sync"
+  "testing"
+  )
+
+const WORKERS = 10
+
+func BenchmarkWithdrawals(b *testing.B) {
+  // Skip N = 1
+  if b.N < WORKERS {
+    return
+  }
+
+  // Add as many pounds as there are iterations in this run
+  fund := NewFund(b.N)
+
+  // Casually assume b.N divides cleanly
+  poundsPerFounder := b.N / WORKERS
+
+  // Waitgroup structs don't need to be initialized
+  // (their zero value is ready to use)
+  // Just declare one and then use it
+  var wg sync.WaitGroup
+
+  for i := 0; i < WORKERS; i++ {
+    // Let the waitgroup know we're adding a goroutine
+    wg.Add(1)
+
+    //Spawn off a founder worker, as a closure
+    go func() {
+      //Mark this worker done when the function finishes
+      defer wg.Done()
+
+      for i := 0; i < poundsPerFounder; i++ {
+        fund.Withdraw(1)
+      }
+    }() // The brackets make this a closure that has been called
+  }
+
+  //Wait for all the workers to finish
+  wg.Wait()
+
+  if fund.Balance() != 0 {
+    b.Error("Balance wasn't zero:", fund.Balance())
+  }
+
+}
 
 func BenchmarkFund(b *testing.B) {
   // Add as many pounds as there are iterations in this run
@@ -13,5 +59,12 @@ func BenchmarkFund(b *testing.B) {
 
   if fund.Balance() != 0 {
     b.Error("Balance wasn't zero:", fund.Balance())
+  }
+
+  func() {
+    resource.Lock()
+    defer resource.Unlock()
+
+    // Do stuff with the resource here...
   }
 }
