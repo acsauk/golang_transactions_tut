78df4b58fcaa602590a37dd861ff6c78f48d54b1
Alex Saunders <acsauk@gmail.com>
2017-05-09 21:51:53 +0100

    Now saves fund.go to commit - oops

---
+++ fund.go
@@ -0,0 +1,23 @@
+package funding
+
+type Fund struct {
+  //Lowercase 'balance' initializes a struct with an unexported (private) balance.
+  balance int
+}
+
+func NewFund(initialBalance int) *fund {
+  // Its possible to return a 'pointer' to a new struct without the need
+  // for it to be on the stack as Go works this out for us
+  return &Fund{
+    balance: initialBalance,
+  }
+}
+
+// Methods being with a 'receiver' - for this case a Fund pointer
+func (f *Fund) Balance() int {
+  return f.balance
+}
+
+func (f *Fund) Withdraw(amount int) {
+  f.balance -= amount
+}
